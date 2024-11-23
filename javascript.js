9f8f0e218dbb2a99f4ce8266842ef518e930a23a
beefshop <152219350+beefshop99@users.noreply.github.com>
2024-11-22 20:45:01 -0600

    Forgot to commit more often. Created template of the functions, having issue with getting the game to receive different input from computer and human inputs to change the rounds outcomes instead of copying.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 
+
+
+// function creates computer choice between rock, paper, scissors
 function getComputerChoice() {
     let result = Math.floor((Math.random() * 3) + 1);
-    console.log(result)
 
         if (result === 1) 
             play = "rock"; 
@@ -10,17 +12,74 @@ function getComputerChoice() {
          else 
             play = "paper";
 
+    console.log('Computer Selection:', play);
     return play;
+   
 }
 
-console.log(getComputerChoice())
 
 
 
+
+//function receives user input between rock, paper, scissors. 
 function getHumanChoice() {
   let a = prompt("Rock, paper or scissors?");
   let b = a.toLowerCase();
-    return b; 
+    console.log('Your Choice:', b); 
+    return b;
 }
 
-console.log(getHumanChoice())
+
+// function to play the game
+
+function playGame() {
+
+let humanScore = 0
+let computerScore = 0
+
+//function plays a round, uses input from getHumanChoice and get ComputerChoice functions. 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors")
+        { return humanScore++, console.log("You win!"); }
+    else if (humanChoice === "rock" && computerChoice === "paper")
+        {return computerScore++, console.log("You Lose!"); }
+    else if (humanChoice === "rock" && computerChoice === "rock" )
+        { console.log("Stalemate!"); }
+    else if (humanChoice === "scissors" && computerChoice === "paper")
+        {return humanScore++, console.log("You win!"); }
+   else if (humanChoice === "scissors" && computerChoice === "rock")
+        { return computerScore++, console.log("You lose!"); }
+    else if (humanChoice === "scissors" && computerChoice === "scissors")
+        { console.log("Stalemate!"); }
+    else if (humanChoice === "paper" && computerChoice === "rock")
+        { return humanScore++, console.log("You win!"); }
+    else if (humanChoice === "paper" && computerChoice === "scissors")
+        { return computerScore++, console.log("You lose!"); }
+    else if (humanChoice === "paper" && computerChoice === "paper")
+        { console.log("Stalemate!"); }
+
+  
+
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);
+
+console.log('Human:' + humanScore);
+console.log('Computer:' + computerScore);
+
+
+
+}
+
+
+
+playGame();
